090884ba1a46f27238f1f82e941a9a78a1440b08
wizardcos <taharajpoot204@gmail.com>
2024-08-13 15:40:18 +0500

    add the routing from react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./components/Home";
 import SearchResult from "./components/SearchResult";
 import { AppContext } from "./utils/ContextApi";
 
 function App() {
-    return <div>Google search app || JS Dev</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/" element={<SearchResult />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,9 @@ import Logo from "../assets/google-logo.png";
 import HomeHeader from "./HomeHeader";
 import SearchInput from "./SearchInput";
 import Footer from "./Footer";
+
 const Home = () => {
-    return <div>Home</div>;
+  return <div>Home </div>;
 };
 
 export default Home;
