6fbd406c7ebcc09d324b4d0456dbed2e9eb1a818
wizardcos <taharajpoot204@gmail.com>
2024-08-14 13:04:41 +0500

    added the result page with search result header completly active

--- src/components/SearchResultHeader.jsx
+++ src/components/SearchResultHeader.jsx
@@ -8,7 +8,45 @@ import { Context } from "../utils/ContextApi";
 import { menu } from "../utils/Constants";
 
 const SearchResultHeader = () => {
-    return <div>Search result header</div>;
+  const [selectedMenu, setselectedMenu] = useState("All");
+  const clickHandler = (menuItem) => {
+    setselectedMenu(menuItem.name);
+  };
+
+  return (
+    <div className="p-[15px] pb-0 md:pl-20 md:pt-7 border-b border-[#ebebeb] flex md:block flex-col items-center sticky top-0 bg-white">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center grow">
+          <img
+            className="hidden md:block w-[92px] mr-10"
+            src={Logo}
+            alt="Google Logo"
+          />
+          <SearchInput from="searchResult" />
+        </div>
+        <div className="hidden md:block">
+          <ProfileIcon />
+        </div>
+      </div>
+      <div className="flex ml-[-12px] mt-3">
+        {menu.map((menu, index) => (
+          <span
+            key={index}
+            className={`flex items-center p-3 text-[#5f6368] cursor-pointer relative ${
+              selectedMenu == menu.name ? "text-[#1a73e8]" : ""
+            }`}
+            onClick={() => clickHandler(menu)}
+          >
+            <span className="hidden md:block mr-2">{menu.icon}</span>
+            <span className="text-sm">{menu.name}</span>
+            {selectedMenu == menu.name && (
+              <span className="h-[3px] w-[calc(100%-20px)] absolute bg-[#1a73e8] bottom-0 left-[10px]" />
+            )}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SearchResultHeader;
