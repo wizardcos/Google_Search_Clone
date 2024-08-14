400ec77f36bd9f79cad60a094f08c07a913a8479
wizardcos <taharajpoot204@gmail.com>
2024-08-14 17:10:20 +0500

    added the pagination functionality

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,12 +1,69 @@
 import { useState, useEffect } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { useParams, useNavigate } from "react-router-dom";
-
 import Logo from "../assets/google-pagination-logo.png";
 import { pagination } from "../utils/Constants";
 
-const Pagination = () => {
-    return <div>Pagination</div>;
+const Pagination = ({ queries }) => {
+  const { query } = useParams();
+  const [page, setPage] = useState(pagination[0].startIndex);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setPage(pagination[0].startIndex);
+  }, [query]);
+
+  const paginationOnClickHandler = (startIndex) => {
+    setPage(startIndex);
+    navigate(`/${query}/${startIndex}`);
+  };
+
+  return (
+    <div className="flex flex-col items-center py-14 max-w-[700px]">
+      <div className="relative text-[#4285f4]">
+        {queries.previousPage && (
+          <div
+            className="absolute left-[-30px] md:left-[-50px] top-[10px]"
+            onClick={() =>
+              paginationOnClickHandler(queries.previousPage[0].startIndex)
+            }
+          >
+            <FiChevronLeft size={20} className="cursor-pointer" />
+            <div className="cursor-pointer absolute left-[-5px] top-[30px] hidden md:block">
+              prev
+            </div>
+          </div>
+        )}
+        <img className="w-[250px] md:w-[300px]" src={Logo} alt="Logo" />
+        {queries.nextPage && (
+          <div
+            className="absolute right-[-30px] md:right-[-50px] top-[10px]"
+            onClick={() =>
+              paginationOnClickHandler(queries.nextPage[0].startIndex)
+            }
+          >
+            <FiChevronRight size={20} className="cursor-pointer" />
+            <div className="cursor-pointer absolute left-[-5px] top-[30px] hidden md:block">
+              Next
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="flex gap-3 text-[#4285f4] text-sm">
+        {pagination.map((p) => (
+          <span
+            key={p.page}
+            onClick={() => paginationOnClickHandler(p.startIndex)}
+            className={`1cursor-pointer ${
+              page === p.startIndex ? "text-black" : ""
+            }`}
+          >
+            {p.page}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Pagination;
